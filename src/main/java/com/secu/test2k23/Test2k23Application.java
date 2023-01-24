6f51e49d41386e39dcbd2e6721da0b34db3584c1
MohammadAbdoulAzizCiss <azizciss6@gmail.com>
2023-01-24 13:28:58 +0000

    First Commit

---
+++ src/main/java/com/secu/test2k23/Test2k23Application.java
@@ -0,0 +1,13 @@
+package com.secu.test2k23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test2k23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test2k23Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/secu/test2k23/Test2k23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secu.test2k23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test2k23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
